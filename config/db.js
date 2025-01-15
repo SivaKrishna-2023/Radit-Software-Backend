6ce975fd6f3dc1d86688b8800d35b069be443a25
Siva Krishna <MTL1018@middlewaretalents.in>
2025-01-15 11:23:09 +0530

    connect to cosmos db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.COSMOS_DB_CONNECTION_STRING);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
